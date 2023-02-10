47927df1578fe340cc16edde886aeac4b1d8d388
Inhwa <2inhwa5@gmail.com>
2023-02-10 12:30:25 +0900

    댓글쓰기 및 댓글쓰기 테스트 완료

---
+++ src/main/java/shop/mtcoding/blog/controller/ReplyController.java
@@ -0,0 +1,42 @@
+package shop.mtcoding.blog.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.Getter;
+import lombok.Setter;
+import shop.mtcoding.blog.handler.ex.CustomException;
+import shop.mtcoding.blog.model.User;
+
+@Controller
+public class ReplyController {
+    @Autowired
+    private HttpSession session;
+
+    @PostMapping("/reply")
+    public String save(ReplySaveReqDto replySaveReqDto) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+        }
+        if (replySaveReqDto.getComment() == null || replySaveReqDto.getComment().isEmpty()) {
+            throw new CustomException("comment를 작성해주세요");
+        }
+        if (replySaveReqDto.getBoardId() == null) {
+            throw new CustomException("boardId가 넘어오지않았습니다");
+        }
+        // 서비스 호출 (replySaveReqDto, principal.gerId())
+        return "redirect:/board/" + replySaveReqDto.getBoardId();
+    }
+
+    @Setter
+    @Getter
+    public static class ReplySaveReqDto {
+        private String comment;
+        private Integer boardId; // Integer : 값이 없으면 null로 들어옴
+    }
+}
