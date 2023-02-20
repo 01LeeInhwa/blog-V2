20b8b88d444ad67cb0ae029850deb317ce65f628
01LeeInhwa <122351923+01LeeInhwa@users.noreply.github.com>
2023-02-20 09:36:07 +0900

    Profile update topic (#3)
    * 프로필 변경 사진 하드디스크에 저장
    
    * 프로필 사진 DB에 저장
    
    * 프로필 사진 ajax 함수 생성

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -1,26 +1,72 @@
 package shop.mtcoding.blog.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import shop.mtcoding.blog.dto.user.UserReq.JoinReqDto;
 import shop.mtcoding.blog.dto.user.UserReq.LoginReqDto;
 import shop.mtcoding.blog.handler.ex.CustomException;
 import shop.mtcoding.blog.model.User;
+import shop.mtcoding.blog.model.UserRepository;
 import shop.mtcoding.blog.service.UserService;
+import shop.mtcoding.blog.util.PathUtil;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     private HttpSession session;
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/user/profileUpdate")
+    public String profileUpdate(MultipartFile profile) {
+
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            return "redirect:/loginForm";
+        }
+
+        if (profile.isEmpty()) {
+            throw new CustomException("사진이 전송되지 않았습니다");
+        }
+
+        // 사진이 아니면 Exception 터트리기
+
+        User userPS = userService.프로필사진수정(profile, principal.getId());
+        session.setAttribute("principal", userPS);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/user/profileUpdateForm")
+    public String profileUpdateForm(Model model) {
+        // 인증
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            return "redirect:/loginForm";
+        }
+        // Ps : 퍼시스턴스 - 영구히 기록되는 것
+        User userPs = userRepository.findById(principal.getId()); // 현재 로그인한 사람의 유저정보로 사진 가지고 오기
+        model.addAttribute("user", userPs);
+        return "user/profileUpdateForm";
+    }
 
     @PostMapping("/join")
     public String join(JoinReqDto joinReqDto) {
